5b49a86272e4e8c029ed96d8ee71afbc43f1f4db
reyuvel <reyuvelkkunjumon@gmail.com>
2025-02-04 22:37:33 +0530

    login page

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Login from './Pages/login'; // Ensure the filename matches the casing
+
+function App() {
+  return (
+    <div className="App">
+      <Login />  {/* Changed <login/> to <Login/> */}
+    </div>
+  );
+}
+
+export default App;
